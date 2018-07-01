5c22dbc515e1e3149417499073f2cb8b7089d2a2
Anatoliy Zaslavskiy <adz@nycitt.com>
2018-07-01 09:46:13 -0700

    before eject

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
 import autoBind from 'react-autobind';
+import YouTube from 'react-youtube';
+import ReactMarkdown from 'react-markdown';
 
 import Header from './Header';
+
+import aboutMe from './sections-text/about-me.md';
 
 import ny from './assets/ny.jpg';
 import sf from './assets/sf.jpg';
@@ -18,8 +22,8 @@ const Wrapper = styled.div`
 `;
 
 const Heading = styled.h2`
-  margin: 0;
   margin-bottom: 5px;
+  margin-top: 20px;
   font-family: 'Anton', sans-serif;
 `;
 
@@ -44,6 +48,21 @@ const AltSection = styled(Section)`
   margin-top: -5px;
   background: #f4f3f5;
   padding-bottom: 20px;
+`;
+
+const FullWidthYouTube = styled(YouTube)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const YouTubeWrapper = styled.div`
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 */
+  padding-top: 25px;
+  height: 0;
 `;
 
 export default class App extends Component {
@@ -71,6 +90,21 @@ export default class App extends Component {
     return (
       <Wrapper menuOpen={menuOpen}>
         <Header onMenuToggle={this.onMenuToggle}/>
+
+        <MainSection>
+          <Heading>Watch My Story</Heading>
+          <YouTubeWrapper>
+            <FullWidthYouTube
+              videoId="z1VNwapOb8E"
+            />
+          </YouTubeWrapper>
+        </MainSection>
+
+        <AltSection>
+          <Heading>About Mee</Heading>
+
+          <ReactMarkdown source={aboutMe} />
+        </AltSection>
 
         <MainSection>
           <Heading>NY -> SF Transplant</Heading>
