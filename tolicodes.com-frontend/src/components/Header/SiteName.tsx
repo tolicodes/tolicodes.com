95ca9459616201e6b60f5bf49eb48394240acfe6
Anatoliy Zaslavskiy <toli@oselot.com>
2021-12-29 15:22:17 -0500

    Storybook and Frontend

---
+++ tolicodes.com-frontend/src/components/Header/SiteName.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface ISiteNameProps {
+    siteLogoSrc: string, siteTagline: string
+}
+
+export const SiteName = ({ siteLogoSrc, siteTagline }: ISiteNameProps) => {
+    return <>Hello World</>
+}
